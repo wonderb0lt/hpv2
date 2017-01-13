4b8d8cd8f458c8915adcbe3d6407c779b716e00c
Patrick Stegmann <code@patrick-stegmann.de>
2017-01-13 22:42:32 +0100

    Added styling and got it to work! Yay!

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import {render} from 'react-dom';
 
 class App extends React.Component {
-    render () {
+    render() {
         return <p> Hello React?</p>;
     }
 }
 
+// Import styling. This makes Webpack embed the compiled CSS into the page (...somehow...)
+import 'style/main.css';
 render(<App/>, document.getElementById('app'));
